0b614954646111e7c3fe8590299555f12a139999
Svetlanakrasyuk <cvmilplus@gmail.com>
2023-11-09 17:37:15 +0700

    install pre-commot

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,24 +5,25 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    if request.method == "GET":
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    elif request.method == "POST":
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['GET', 'PUT', 'DELETE'])
+
+
+@api_view(["GET", "PUT", "DELETE"])
 def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
@@ -32,17 +33,17 @@ def snippet_detail(request, pk, format=None):
     except Snippet.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+    if request.method == "GET":
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    elif request.method == "PUT":
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
